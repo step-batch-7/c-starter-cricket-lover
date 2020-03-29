0a63e175b0bab0cf31770051ba8aeda0e20492c3
phaneendra <phaneendraandukuri@gmail.com>
2020-03-29 12:14:19 +0530

    added average_of_three function

--- basic_functions.c
+++ basic_functions.c
@@ -4,6 +4,7 @@ unsigned char is_even(int);
 unsigned char is_odd(int);
 int square(int);
 int cube(int);
+float average_of_three(int, int, int);
 
 unsigned char is_even(int num)
 {
@@ -25,13 +26,23 @@ int cube(int num)
 	return num * square(num);
 }
 
+float average_of_three(int num1, int num2, int num3)
+{
+	return (num1 + num2 + num3) / 3.00;
+}
+
 int main(void)
 {
 	int number;
+	int num1, num2, num3;
 	printf("please enter a number:\n");
 	scanf("%d", &number);
 	printf("%d is %s\n", number, is_even(number) ? "even" : "not even");
 	printf("%d is %s\n", number, is_odd(number) ? "odd" : "not odd");
 	printf("square of %d is %d\n", number, square(number));
 	printf("cube of %d is %d\n", number, cube(number));
+	printf("enter three numbers:\n");
+	scanf("%d%d%d", &num1, &num2, &num3);
+	printf("Average of %d,%d,%d is %f\n", num1, num2, num3, average_of_three(num1,num2,num3));
+	return 0;
 }
