6a76e0fbdd181b2b4c95f05a1675b9a0de3d9440
phaneendra <phaneendraandukuri@gmail.com>
2020-03-30 12:58:04 +0530

    added print_odd_series function

--- loops.c
+++ loops.c
@@ -2,6 +2,8 @@
 
 unsigned long int factorial(int);
 int fibonacci_series(int);
+unsigned char is_even(int);
+int print_odd_series(int);
 
 unsigned long int factorial(int num)
 {
@@ -31,6 +33,20 @@ for (int index = 0; index < num; index++) {
 return 0;
 }
 
+unsigned char is_even(int num)
+{
+	return num % 2 == 0;
+}
+
+int print_odd_series(int num)
+{
+	for(int index = 1; index <= num; index++)
+	{
+		is_even(index) || printf("%d ", index);
+	}
+	return 0;
+}
+
 int main(void)
 {
 	int number;
@@ -38,4 +54,5 @@ int main(void)
 	scanf("%d", &number);
 	printf("Factorial of %d is %lu\n", number, factorial(number));
 	fibonacci_series(number);
+	print_odd_series(number);
 }
